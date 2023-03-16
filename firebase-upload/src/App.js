d0e53549b94e9774dcb7100ccdb811ca72545b55
Becky Blake <ryblake2@illinois.edu>
2023-03-16 16:00:20 -0500

    added arrays for type of clothing and displayed them by type

--- firebase-upload/src/App.js
+++ firebase-upload/src/App.js
@@ -6,41 +6,48 @@ function App() {
   // State to store uploaded file
   const [file, setFile] = useState(""); // progress
   const [percent, setPercent] = useState(0); // Handle file upload event and update state
-  const [url, setUrl] = useState("");
-  // const starsRef = ref(storage, `/files/${file.name}`);
-  const starsRef = ref(storage, 'gs://king-booleans-virtual-closet.appspot.com/files/download.jpg');
-  getDownloadURL(starsRef).then((url) => {
-    setUrl(url);
-  });
-  
-  // listAll(listRef)
-  // .then((res) => {
-  //   res.prefixes.forEach((folderRef) => {
-  //     // All the prefixes under listRef.
-  //     // You may call listAll() recursively on them.
-  //   });
-  //   res.items.forEach((itemRef) => {
-  //     // All the items under listRef.
-  //   });
-  // }).catch((error) => {
-  //   // Uh-oh, an error occurred!
-  // });
 
+  const [shirts, setShirts] = useState([]);
+  const [pants, setPants] = useState([]);
+  const [shoes, setShoes] = useState([]);
+  const [hats, setHats] = useState([]);
 
-  const [items, setItems] = useState([]);
   useEffect(() => {
-    const listRef = ref(storage, 'gs://king-booleans-virtual-closet.appspot.com/files');
-    listAll(listRef)
-      .then(async (res) => {
-        setItems(await Promise.all(res.items.map(item => getDownloadURL(item))));
+    const shirtsRef = ref(storage, 'gs://king-booleans-virtual-closet.appspot.com/shirts');
+    const pantsRef = ref(storage, 'gs://king-booleans-virtual-closet.appspot.com/pants');
+    const shoesRef = ref(storage, 'gs://king-booleans-virtual-closet.appspot.com/shoes');
+    const hatsRef = ref(storage, 'gs://king-booleans-virtual-closet.appspot.com/hats');
+  
+    Promise.all([
+      listAll(shirtsRef),
+      listAll(pantsRef),
+      listAll(shoesRef),
+      listAll(hatsRef),
+    ])
+      .then(([shirtsRes, pantsRes, shoesRes, hatsRes]) => {
+        const fetchDownloadUrls = async (items) => {
+          return Promise.all(items.map((item) => getDownloadURL(item)));
+        };
+        Promise.all([
+          fetchDownloadUrls(shirtsRes.items),
+          fetchDownloadUrls(pantsRes.items),
+          fetchDownloadUrls(shoesRes.items),
+          fetchDownloadUrls(hatsRes.items),
+        ])
+          .then(([shirts, pants, shoes, hats]) => {
+            setShirts(shirts);
+            setPants(pants);
+            setShoes(shoes);
+            setHats(hats);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
       .catch((error) => {
         console.log(error);
-        // Uh-oh, an error occurred!
-
       });
   }, []);
-
 
   
   function handleChange(event) {
@@ -72,9 +79,16 @@ function App() {
       <button onClick={handleUpload}>Upload to Firebase</button>
       <p>{percent} "% done"</p>
   
-      <img src={url} alt="uploaded"  />
+      {/* <img src={url} alt="uploaded"  /> */}
 
-      {items.map((item, i) => <img src={item} alt="this" key={i}/>)}
+      <h1>Shirts</h1>
+      {shirts.map((shirts, i) => <img src={shirts} alt="this" key={i}/>)}
+      <h1>Pants</h1>
+      {pants.map((pants, i) => <img src={pants} alt="this" key={i}/>)}
+      <h1>Shoes</h1>
+      {shoes.map((shoes, i) => <img src={shoes} alt="this" key={i}/>)}
+      <h1>Hats</h1>
+      {hats.map((hats, i) => <img src={hats} alt="this" key={i}/>)}
 
     </div >
 
