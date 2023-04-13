75ea3ef9e02b80ada27c1ec921b407439b4287c0
Becky Blake <ryblake2@illinois.edu>
2023-04-13 18:26:55 -0500

    working on pulling data from firebase

--- firebase-upload/src/pages/past-outfits.js
+++ firebase-upload/src/pages/past-outfits.js
@@ -11,75 +11,62 @@ import Carousel, { CarouselItem } from "../carousel";
 
 
 const PastOutfits = () => {
-    const [shirts, setShirts] = useState([]);
-    const [pants, setPants] = useState([]);
-    const [shoes, setShoes] = useState([]);
-    const [hats, setHats] = useState([]);
-    
-    useEffect(() => {
-        const shirtsRef = ref(
-        storage,
-        "gs://king-booleans-virtual-closet.appspot.com/shirts"
-        );
-        const pantsRef = ref(
-        storage,
-        "gs://king-booleans-virtual-closet.appspot.com/pants"
-        );
-        const shoesRef = ref(
-        storage,
-        "gs://king-booleans-virtual-closet.appspot.com/shoes"
-        );
-        const hatsRef = ref(
-        storage,
-        "gs://king-booleans-virtual-closet.appspot.com/hats"
-        );
-    
-        Promise.all([
-        listAll(shirtsRef),
-        listAll(pantsRef),
-        listAll(shoesRef),
-        listAll(hatsRef),
-        ])
-        .then(([shirtsRes, pantsRes, shoesRes, hatsRes]) => {
-            const fetchDownloadUrls = async (items) => {
-            return Promise.all(items.map((item) => getDownloadURL(item)));
-            };
-            Promise.all([
-            fetchDownloadUrls(shirtsRes.items),
-            fetchDownloadUrls(pantsRes.items),
-            fetchDownloadUrls(shoesRes.items),
-            fetchDownloadUrls(hatsRes.items),
-            ])
-            .then(([shirts, pants, shoes, hats]) => {
-                setShirts(shirts);
-                setPants(pants);
-                setShoes(shoes);
-                setHats(hats);
+
+  const [outfits, setOutfits] = useState([]);
+
+  useEffect(() => {
+    const storageRef = ref(storage, "gs://king-booleans-virtual-closet.appspot.com/past-outfits");
+
+    storageRef.listAll().then((result) => {
+      result.items.forEach((itemRef) => {
+        itemRef.getDownloadURL().then((url) => {
+          fetch(url)
+            .then((response) => response.text())
+            .then((data) => {
+              const { hat, shirt, pant, shoe, date } = JSON.parse(data);
+              setOutfits((prevOutfits) => [
+                ...prevOutfits,
+                { hat, shirt, pant, shoe, date },
+              ]);
             })
-            .catch((error) => {
-                console.log(error);
-            });
-        })
-        .catch((error) => {
-            console.log(error);
+            .catch((error) => console.log(error));
         });
-    }, []);
+      });
+    }).catch((error) => console.log(error));
+  }, []);
+
+  
 
 
-    return (
-        <div>
-        <h1 style={{ textAlign: "center" }}>Past Outfits</h1>
-        <h3
-            style={{
-            textAlign: "center",
-            fontWeight: "600",
-            marginBottom: "5%",
-            }}
-        >
-            View some of your past outfits!
-        </h3>
-        </div>
-    );
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Past Outfits</h1>
+      <h3
+        style={{
+          textAlign: "center",
+          fontWeight: "600",
+          marginBottom: "5%",
+        }}
+      >
+        View some of your past outfits!
+      </h3>
+        
+    
+      <Carousel>
+        {outfits.map((outfit, index) => (
+          <CarouselItem key={index}>
+            {" "}
+            <div>
+              <img src={outfit.hat} alt="hat" />
+              <img src={outfit.shirt} alt="shirt" />
+              <img src={outfit.pant} alt="pants" />
+              <img src={outfit.shoe} alt="shoes" />
+              <p>Date: {outfit.date}</p>
+            </div>
+          </CarouselItem>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
-
 export default PastOutfits;
